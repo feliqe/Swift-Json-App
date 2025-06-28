f3ab84bda0abf88591f1eaee10787b536bee461b
feliqe <feliqe.cerda@gmail.com>
2025-06-28 13:02:35 -0400

    Initial Commit

---
+++ Json-StarWars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Json-StarWars
+//
+//  Created by Feliqe Silva on 28-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Json-StarWars/Json_StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Json_StarWarsApp.swift
+//  Json-StarWars
+//
+//  Created by Feliqe Silva on 28-06-25.
+//
+
+import SwiftUI
+
+@main
+struct Json_StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
